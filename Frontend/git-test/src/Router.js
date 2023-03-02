ce9509c8b949e63261b0568b7013ed042c2e54bd
zunyange <hiillun00553300@gmail.com>
2023-03-02 17:31:09 +0900

    ADD : 두번째 PR

--- Frontend/git-test/src/Router.js
+++ Frontend/git-test/src/Router.js
@@ -1,12 +1,13 @@
-import React from 'react';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import App from './App';
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
+import Main from "../src/pages/Main/Main";
 
 export default function Router() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<App />} />
+        <Route path="/main" element={<Main />} />
       </Routes>
     </BrowserRouter>
   );
